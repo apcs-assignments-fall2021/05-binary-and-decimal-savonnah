47b40f4e7ab4869e73f5b44448dc090188a5169e
savonnahwong <23swong@athenian.org>
2021-09-14 19:19:10 -0700

    Attempted to Finish but decimaltoBinary doesnt work

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -8,8 +8,16 @@ public class MyMain {
     // Ex. binaryToDecimal("1010") => 10 (ten)
     //     binaryToDecimal("110010") => 50
     public static int binaryToDecimal(String binary) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int num = Integer.parseInt(binary); //changes string to int
+        int sum = 0; //this is the starting that decimal numbers found will be added to
+        int count = 0; // will count the power of the digit as code iterates thought the number
+        while(num>0){
+            int lastdigit = num%10; //gets last digit of int
+            num=num/10; //chops off last digit
+            sum += lastdigit*Math.pow(2,count); //gets the last digit and multiplies it by 2 to the power of the place the digit is from right to left
+            count++; // increases the power each time goes through one iteration
+        }
+        return sum; // the decimal
     }
 
     // Given a decimal number (e.g. 152) as input, and returns a String
@@ -17,13 +25,27 @@ public class MyMain {
     // Ex. decimalToBinary(7) => "111"
     //     decimalToBinary(152) => "10011000"
     public static String decimalToBinary(int decimal) {
-        // REPLACE WITH YOUR CODE
-        return "";
+        int count = 0; // will count the power of the digit as code iterates thought the number
+        String str = "";
+        while (decimal>0){
+            int remain = decimal%2;
+            decimal=decimal/2;
+            str = "" + remain;
+        }
+        return str;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("Type in a number in binary format: ");
+        String inputstr = scan.next();
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(inputstr));
+        System.out.println("Type in a number in decimal format: ");
+        int inputdec = scan.nextInt();
+        System.out.println("That is equal to the decimal value: " + decimalToBinary(inputdec));
+
+
+        scan.close();
     }
 }
